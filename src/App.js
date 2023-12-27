7eb57b3d71785da369430e0fa2be95d239a3622a
Diego Carvalho <108098475+DiegoCrvlho@users.noreply.github.com>
2023-12-27 17:51:24 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import "./styles.css";
+
+import { useState } from "react";
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus."
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text:
+      "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus."
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text:
+      "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!"
+  }
+];
+
+export default function App() {
+  return (
+    <div>
+      <Accordion data={faqs} />
+    </div>
+  );
+}
+
+function Accordion({ data }) {
+  const [curr, setCurr] = useState(null);
+
+  return (
+    <div className="accordion">
+      {faqs.map((el, i) => (
+        <AccordionItem
+          num={i + 1}
+          title={el.title}
+          text={el.text}
+          key={el.title}
+          curr={curr}
+          onClick={setCurr}
+        />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ num, title, text, curr, onClick }) {
+  const isOpen = num === curr;
+
+  function handleItem() {
+    onClick(isOpen ? null : num);
+  }
+  return (
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleItem}>
+      <p className="number">{num < 9 ? `0${num}` : num + 1}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{text}</div>}
+    </div>
+  );
+}
